9aa3ed4b3815b8001068f016ec67934cd41b9379
Ferdous Ahmed <himibaba10@gmail.com>
2024-08-13 20:04:16 +0600

    Created a product model with sequelize

--- models/product.js
+++ models/product.js
@@ -1,63 +1,29 @@
-const Cart = require("./cart");
-const db = require("../utils/database");
-class Product {
-  constructor(title, price, imageUrl, description) {
-    this.id = Math.floor(Math.random() * 10000).toString();
-    this.title = title;
-    this.price = price;
-    this.imageUrl = imageUrl;
-    this.description = description;
-  }
+const Sequelize = require("sequelize");
+const sequelize = require("../utils/database");
 
-  save() {
-    return db.execute(
-      "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
-
-  static findById(id) {
-    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
-  }
-
-  static update(productData, cb) {
-    getProductsFromFile((products) => {
-      const updatedProductIndex = products.findIndex(
-        (prod) => prod.id === productData.id
-      );
-      const updatedProducts = [...products];
-      updatedProducts[updatedProductIndex] = productData;
-      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-        if (err) {
-          console.log("Error writing product data: ", err);
-          return;
-        } else {
-          cb();
-        }
-      });
-    });
-  }
-
-  static delete(id, cb) {
-    getProductsFromFile((products) => {
-      const product = products.find((prod) => prod.id === id);
-      const updatedProducts = products.filter((prod) => prod.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-        if (err) {
-          console.log("Error deleting product data: ", err);
-          return;
-        } else {
-          Cart.deleteProduct(product.id, product.price, () => {
-            cb();
-          });
-        }
-      });
-    });
-  }
-}
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
 module.exports = Product;
