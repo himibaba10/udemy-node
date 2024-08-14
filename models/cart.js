70d1b39032679c19020734a50022dcbe92dbc2e3
Ferdous Ahmed <himibaba10@gmail.com>
2024-08-15 02:15:00 +0600

    Created the Cart model

--- models/cart.js
+++ models/cart.js
@@ -1,70 +1,13 @@
-const fs = require("fs");
-const path = require("path");
+const Sequelize = require("sequelize");
+const sequelize = require("../utils/database");
 
-const p = path.join(process.cwd(), "data", "cart.json");
-
-class Cart {
-  static addProduct(id, price, cb) {
-    fs.readFile(p, (err, cartData) => {
-      let cart = { products: [], totalPrice: 0 };
-
-      if (!err) {
-        cart = JSON.parse(cartData);
-      }
-
-      const existingProductIndex = cart.products.findIndex(
-        (prod) => prod.prodId === id
-      );
-      const existingProduct = cart.products[existingProductIndex];
-      if (existingProduct) {
-        existingProduct.qty += 1;
-        cart.products[existingProductIndex] = existingProduct;
-      } else {
-        cart.products = [...cart.products, { prodId: id, qty: 1 }];
-      }
-
-      cart.totalPrice += Number((+price).toFixed(2));
-
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
-        if (err) {
-          console.log("Error writing cart data: ", err);
-        } else {
-          cb(cart);
-        }
-      });
-    });
-  }
-
-  static deleteProduct(id, price, cb) {
-    fs.readFile(p, (err, cartData) => {
-      const cart = JSON.parse(cartData);
-      const product = cart.products.find((prod) => prod.prodId === id);
-
-      const updatedCart = { ...cart };
-
-      updatedCart.products = updatedCart.products.filter(
-        (prod) => prod.prodId !== id
-      );
-      updatedCart.totalPrice = updatedCart.totalPrice - product.qty * price;
-
-      fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
-        if (err) {
-          console.log("Error deleting cart product: ", err);
-        } else {
-          cb();
-        }
-      });
-    });
-  }
-
-  static getProducts(cb) {
-    fs.readFile(p, (err, cartData) => {
-      if (err) {
-        console.log("Error getting cart products: ", err);
-      }
-      cb(JSON.parse(cartData));
-    });
-  }
-}
+const Cart = sequelize.define("cart", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+});
 
 module.exports = Cart;
