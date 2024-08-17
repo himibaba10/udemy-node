0cbb3865708bb85f22395f77bbbfc48e3971f53a
Ferdous Ahmed <himibaba10@gmail.com>
2024-08-17 15:45:45 +0600

    Installed and connected mongodb database successfully

--- app.js
+++ app.js
@@ -8,16 +8,10 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 const { getError } = require("./controllers/error");
-const sequelize = require("./utils/database");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+const { mongoConnect } = require("./utils/database");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -34,51 +28,13 @@ app.use((req, res, next) => {
     });
 });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(getError);
 
-// Relations between the models
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-Product.belongsToMany(Order, { through: OrderItem });
-
-let loggedInUser;
-sequelize
-  // .sync({ force: true }) //for development
-  .sync() //for production
-  .then(() => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "Ferdous", email: "himibaba10@gmail.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    loggedInUser = user;
-    return user.getCart();
-  })
-  .then((cart) => {
-    if (!cart) {
-      return loggedInUser.createCart();
-    }
-    return cart;
-  })
-  .then(() => {
-    app.listen(3000, () => {
-      console.log("App is running on port 3000 and connected to sequelize");
-    });
-  })
-  .catch((err) => {
-    console.log(err);
+mongoConnect(() => {
+  app.listen(3000, () => {
+    console.log("App is running on port 3000 and connected to Mongodb");
   });
+});
