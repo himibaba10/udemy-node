2214bbbe7dc83b44bae68f0bc57c35aec3c83c3f
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-08-29 18:30:58 +0600

    Installed and connected mongoose to the app

--- app.js
+++ app.js
@@ -8,34 +8,38 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 
 const { getError } = require("./controllers/error");
-const { mongoConnect } = require("./utils/database");
-const User = require("./models/user");
+// const User = require("./models/user");
+const { default: mongoose } = require("mongoose");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("66c324fc0ba52df2b0303928")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+// app.use((req, res, next) => {
+//   User.findById("66c324fc0ba52df2b0303928")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(getError);
 
-mongoConnect(() => {
-  app.listen(3000, () => {
-    console.log("App is running on port 3000 and connected to Mongodb");
+mongoose
+  .connect(
+    "mongodb+srv://himibaba10:PDSc0wmxY1wiVn65@cluster0.jtbd7.mongodb.net/node-complete?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then((result) => {
+    app.listen(3000, () => {
+      console.log("App is running on port 3000 and connected to Mongoose");
+    });
   });
-});
