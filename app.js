4db5927554f75aa3156a3ec26a1f5df212f02dde
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-08-30 16:44:00 +0600

    added functionality to have a user on the whole application

--- app.js
+++ app.js
@@ -12,22 +12,22 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const { getError } = require("./controllers/error");
-// const User = require("./models/user");
+const User = require("./models/user");
 const { default: mongoose } = require("mongoose");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("66c324fc0ba52df2b0303928")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
+app.use((req, res, next) => {
+  User.findById("66d1a20cda701462bae81c4f")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -39,6 +39,16 @@ mongoose
     "mongodb+srv://himibaba10:PDSc0wmxY1wiVn65@cluster0.jtbd7.mongodb.net/node-complete?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const newUser = new User({
+          name: "Ferdous Ahmed",
+          email: "himibaba10@gmail.com",
+          cart: { items: [] },
+        });
+        return newUser.save();
+      }
+    });
     app.listen(3000, () => {
       console.log("App is running on port 3000 and connected to Mongoose");
     });
