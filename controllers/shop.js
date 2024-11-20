9252d2e1d3d60384cba09147c750541070ea0afc
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-11-21 02:50:10 +0600

    invoices are now downloaded as stream

--- controllers/shop.js
+++ controllers/shop.js
@@ -170,20 +170,25 @@ const getInvoice = (req, res, next) => {
       return next(new Error("Unauthorized access."));
     }
 
-    fs.readFile(invoicePath, (err, data) => {
-      if (err) {
-        console.log(err);
-        return next(err);
-      }
+    // fs.readFile(invoicePath, (err, data) => {
+    //   if (err) {
+    //     console.log(err);
+    //     return next(err);
+    //   }
 
-      //setting header so we know it is a pdf and can be downloaded as pdf
-      res.setHeader("Content-Type", "application/pdf");
+    //   //setting header so we know it is a pdf and can be downloaded as pdf
+    //   res.setHeader("Content-Type", "application/pdf");
 
-      //attachment means download, inline means open the file in the browser
-      res.setHeader("Content-Disposition", `inline; filename="${invoiceName}"`);
+    //   //attachment means download, inline means open the file in the browser
+    //   res.setHeader("Content-Disposition", `inline; filename="${invoiceName}"`);
 
-      res.send(data);
-    });
+    //   res.send(data);
+    // });
+
+    const file = fs.createReadStream(invoicePath);
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", `inline; filename="${invoiceName}"`);
+    file.pipe(res);
   });
 };
 
